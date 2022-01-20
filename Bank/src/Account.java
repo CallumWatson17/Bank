8120f898d8c2117ec3be6e6809707ba158059861
CallumWatson17 <callumwatson1@sky.com>
2022-01-20 14:59:07 +0000

    First Commit

---
+++ Bank/src/Account.java
@@ -0,0 +1,37 @@
+
+public class Account {
+	
+	private int accountNumber;
+	private int pin;
+	private double avaiablebalance;
+	private double totalBalance;
+	private int admin;
+	private String userName;
+	
+	public Account(String userName, int AccountNumber, int thePin, double theAvaiableBalance, double theTotalBalance, int isAdmin) {
+		setUsername(userName);
+		setAccountNumber(theAccountNumber);
+		setPin(thePin);
+		set
+	}
+	
+	public boolean validatePin(int userPin) {
+		if(userPin == getPin()) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public double getAvailableBalance()
+	{
+		return avaiablebalance;
+	}
+	
+	public double getTotalBalance() {
+		return totalBalance;
+	}
+	
+
+}
---
+++ Bank/src/Bank.java
@@ -0,0 +1,49 @@
+
+public class Bank {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String name;
+		int num = 2;
+		
+		
+		Bank bank1 = new Bank("Hello", 21);
+		
+		System.out.println(bank1);
+		
+		
+		
+		bank1.hello();
+		
+		System.out.println(equals(5));
+		
+		
+		
+		
+		
+		
+	
+		
+	}
+
+	public static boolean equals(String num) {
+		if(String ==   2) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	
+	public Bank(String name, int num) {
+		name = name;
+		num = num;
+		
+	}
+	
+	public void hello() {
+		System.out.println("Hi");
+	}
+
+}
